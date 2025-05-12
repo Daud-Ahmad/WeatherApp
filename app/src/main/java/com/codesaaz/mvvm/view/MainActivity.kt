f76a5444268c88cbd1b4dc5d42e73ca3c2f41e8d
Daud <40161715+Daud-Ahmad@users.noreply.github.com>
2025-05-12 23:10:28 +0500

    improve the ui design of weather screen

--- app/src/main/java/com/codesaaz/mvvm/view/MainActivity.kt
+++ app/src/main/java/com/codesaaz/mvvm/view/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.codesaaz.mvvm.view
 
 import android.os.Bundle
+import android.view.View.GONE
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -9,7 +10,6 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.Observer
 import com.codesaaz.mvvm.R
 import com.codesaaz.mvvm.viewmodel.WeatherViewmodel
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,15 +23,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.weather_layout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
         val cityInput = findViewById<EditText>(R.id.etCity)
-        val btnFetch = findViewById<Button>(R.id.btnFetch)
-        val tvResult = findViewById<TextView>(R.id.tvResult)
+        val btnFetch = findViewById<Button>(R.id.btnSearch)
+        val tvLocation = findViewById<TextView>(R.id.tvLocation)
+        val tvTemp = findViewById<TextView>(R.id.tvTemp)
+        val tvFeelsLike = findViewById<TextView>(R.id.tvFeelsLike)
 
         btnFetch.setOnClickListener {
             val city = cityInput.text.toString()
@@ -41,17 +43,25 @@ class MainActivity : AppCompatActivity() {
         }
 
         viewModel.weatherData.observe(
-            this,{
-                it?.let {
-                    tvResult.text = "City: ${it.location.name}\nTemperature: ${it.current.temp_c}°C"
-                }
+            this
+        ) {
+            it?.let {
+                val location =
+                    "Location: ${it.location.name}, ${it.location.region}, ${it.location.country}"
+                val tem = "Temperature: ${it.current.temp_c} °C"
+                val feelLike = "Feels Like: ${it.current.feelslike_c}  °C"
+                tvLocation.text = location
+                tvTemp.text = tem
+                tvFeelsLike.text = feelLike
             }
-        )
+        }
 
         viewModel.error.observe(
-            this, {
-                tvResult.text = it
-            }
-        )
+            this
+        ) {
+            tvLocation.text = it
+            tvTemp.visibility = GONE
+            tvFeelsLike.visibility = GONE
+        }
     }
 }
